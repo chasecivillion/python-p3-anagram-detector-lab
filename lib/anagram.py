ac1b50320b5a2281df1acce9e88c006af3f2839e
Beau Kim <beaukim3@gmail.com>
2023-03-15 22:41:08 -0400

    finished

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import ipdb
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, list_words):
+        matching_word_list = []
+        for word in list_words:
+            if sorted([letter for letter in word]) == self.word_letters:
+                matching_word_list.append(word)
+        return matching_word_list
